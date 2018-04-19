7913633f2c983372d98ec233dc6c67b4022e6e30
Peter Martinson <>
2018-04-19 02:13:04 +0000

    complete credit

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -1,66 +1,45 @@
+/**
+ * Runs credit card numbers through a validation algorithm
+ * 2018 (c) Peter Martinson
+ * 
+ * 1. Multiply every other digit by 2, starting at the penultimate digit
+ * 2. Add all digits of the above products together
+ * 3. Add the above sum to the sum of all the other digits
+ * 4. If the last digit of this final sum is zero, the number is valid
+ * 
+ * Note:  For step 1, the maxiumum product is 9*9=18.  So, for products greater
+ * than 9, the sum of digits is the same as the last digit minus nine.  For
+ * example, take 15.  1+5 = 6 = 15-9.
+ */
 #include <stdio.h>
-/**
-
-:) credit.c exists.
-:) credit.c compiles.
-:( identifies 378282246310005 as AMEX expected "AMEX\n", not "\nResult (15 di..."
-:( identifies 371449635398431 as AMEX expected "AMEX\n", not "\nResult (15 di..."
-:( identifies 5555555555554444 as MASTERCARD expected "MASTERCARD\n", not "\nResult (16 di..."
-:( identifies 5105105105105100 as MASTERCARD expected "MASTERCARD\n", not "\nResult (16 di..."
-:( identifies 4111111111111111 as VISA expected "VISA\n", not "\nResult (16 di..."
-:( identifies 4012888888881881 as VISA expected "VISA\n", not "\nResult (16 di..."
-:( identifies 1234567890 as INVALID expected "INVALID\n", not "\nResult (10 di..."
-:( identifies 369421438430814 as INVALID expected "INVALID\n", not "\nResult (15 di..."
-:( identifies 4062901840 as INVALID expected "INVALID\n", not "\nResult (10 di..."
-:( identifies 5673598276138003 as INVALID expected "INVALID\n", not "\nResult (16 di..."
-:( identifies 4111111111111113 as INVALID expected "INVALID\n", not "\nResult (16 di..."
-:) rejects a non-numeric input of "foo"
-:) rejects a non-numeric input of ""
-
-*/
-/**
- * Specifications:
-   AMEX - 15 digits
-     start with 34 or 37
-   MC - 16 digits
-     start with 51-55
-   Visa - 13 or 16 digits
-     start with 4
-
-   Checksums:
-   1. Multiply every other digit by 2, starting with second-last digit
-   2. Add those products together
-   3. Add that sum to the sum of all the other digits
-   4. If last digit is 0, the number is valid
-*/
 
 int * getCardNumber(void);
 int validateCardNumber(int *);
 
 int main(void)
 {
-
-  int i, digits, *card, valid_number = 0;
+  int digits, *card, valid_number = 0;
 
   card = getCardNumber();
   digits = card[0];
   valid_number = validateCardNumber(card);
 
-  printf("\n");
+  if (valid_number != 0)
+    printf("INVALID\n");
 
-  if ( valid_number != 0 )
-    printf("INVALID - checksum = %d\n", valid_number);
-  else if ( card[1] == 3 && (card[2] == 4 || card[2] == 7) && digits == 15 )
+  else if (card[1] == 3 && (card[2] == 4 || card[2] == 7) && digits == 15)
     printf("AMEX\n");
-  else if ( card[1] == 5 && (card[2] >= 1 && card[2] <= 5) && digits == 16 )
-    printf("MC\n");
-  else if ( card[1] == 4 && (digits == 13 || digits == 16 ) )
-    printf("Visa\n");
+
+  else if (card[1] == 5 && (card[2] >= 1 && card[2] <= 5) && digits == 16)
+    printf("MASTERCARD\n");
+
+  else if (card[1] == 4 && (digits == 13 || digits == 16))
+    printf("VISA\n");
+
   else
     printf("INVALID\n");
 
   return 0;
-  
 }
 
 int * getCardNumber()
@@ -69,24 +48,22 @@ int * getCardNumber()
   printf("Number: ");
 
   int c, digits = 0, redo_flag = 0, *return_pointer;
-  static int card_number[16];
+  static int card_number[17];
 
-  while ( (c=getchar()) != EOF && c != '\n' )
+  while ((c=getchar()) != EOF && c != '\n')
   {
     if (c < '0' || c > '9' || digits > 15)
-    { // If input is not a number, or too many digits
+    {
       redo_flag = 1;
     }
     else
-    { // Otherwise, push onto the array
+    {
       card_number[++digits] = c - 48;
     }
   }
 
-  // First element of array stores length of number
   card_number[0] = digits;
 
-  // Hand address of first element to a pointer
   return_pointer = card_number;
 
   // if the number is bad, recurse
@@ -99,19 +76,21 @@ int * getCardNumber()
 
 }
 
-int validateCardNumber(int *p) {
-  int length = p[0];
-  int first_sum = 0;
-  int final_sum = 0;
-  int i;
+int validateCardNumber(int *p)
+{
+  int length = p[0], sum = 0, i;
 
-    printf("\np[i] p[i+1] first_sum final_sum\n");
-  for (i = length - 1; i > 0; i = i - 2)
+  for (i = length - 1; i >= 0; i = i - 2)
   {
-    first_sum += p[i] * 2;
-    final_sum += p[i + 1];
-    printf("%d %d %d %d\n", p[i], p[i+1], first_sum, final_sum);
+    if (i != 0) // This prevents first element from being counted
+    {
+      if (p[i] * 2 >= 10)
+        sum += p[i] * 2 - 9;
+      else
+        sum += p[i] * 2;
+    }
+    sum += p[i + 1];
   }
     
-  return (first_sum + final_sum)%10;
+  return sum % 10;
 }
