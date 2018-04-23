6d0e1c0677df74e02dd2db3080da09d532575969
Peter Martinson <peter.j.martinson@gmail.com>
2018-04-23 17:16:11 -0400

    Mess around

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main(int argc, const char *argv[])
 {
   int key = (int) strtol(argv[1], NULL, 10);
 
-  printf("key: %d\n", key);
+  printf("key: %d\n", key + 5);
 
   return 0;
 }
