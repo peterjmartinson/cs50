297879504677b0a78df260e24bbe9b98a9987686
Peter Martinson <peter.j.martinson@gmail.com>
2018-04-24 17:13:47 -0400

    Print the resulting string

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -1,3 +1,8 @@
+/**
+ * Produces a Caesar encryption
+ * 2018 (c) Peter Martinson
+ * 
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,6 +10,8 @@ int isUpper(char letter);
 int isLower(char letter);
 void shiftUpper(char *letter, int key);
 void shiftLower(char *letter, int key);
+/* int * getString(void); */
+
 // A-Z = 65-90
 // a-z = 97-122
 
@@ -16,7 +23,6 @@ int main(int argc, const char *argv[])
 
   for (i = 0; str[i] != '\0'; i++)
   {
-    printf("%c (%d) ", str[i], str[i]);
     if (isUpper(str[i]))
     {
       shiftUpper(str + i, key);
@@ -27,10 +33,10 @@ int main(int argc, const char *argv[])
     }
     else
       ;
-
   }
 
-  printf("\n");
+  printf("new string: %s\n", str);
+
   return 0;
 }
 
@@ -51,11 +57,46 @@ int isLower(char letter) {
 void shiftUpper(char *letter, int key) {
   char l = *letter;
   char new_letter = (l - 65 + key)%26 + 65;
-  printf("shifting [%c] to [%c]\n", l, new_letter);
+  *letter = new_letter;
+  /* printf("shifting [%c] to [%c]\n", l, new_letter); */
 }
 
 void shiftLower(char *letter, int key) {
   char l = *letter;
   char new_letter = (l - 97 + key)%26 + 97;
-  printf("shifting [%c] to [%c]\n", l, new_letter);
+  *letter = new_letter;
 }
+
+/* int * getString(void) */
+/* { */
+
+/*   printf("Phrase: "); */
+
+/*   int c, digits = 0, redo_flag = 0, *return_pointer; */
+/*   static int card_number[17]; */
+
+/*   while ((c=getchar()) != EOF && c != '\n') */
+/*   { */
+/*     if (c < '0' || c > '9' || digits > 15) */
+/*     { */
+/*       redo_flag = 1; */
+/*     } */
+/*     else */
+/*     { */
+/*       card_number[++digits] = c - 48; */
+/*     } */
+/*   } */
+
+/*   card_number[0] = digits; */
+
+/*   return_pointer = card_number; */
+
+/*   // if the number is bad, recurse */
+/*   if (redo_flag == 1) */
+/*   { */
+/*     return_pointer = getCardNumber(); */
+/*   } */
+
+/*   return return_pointer; */
+
+/* } */
